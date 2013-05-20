fce0dfdbb38412cd0b785d32ddccdd1122e9b6c1
Laurent <lbhomeadmin@gmail.com>
2013-05-20 19:11:58 +0200

    Added comments

--- app/helpers/open_lists/application_helper.rb
+++ app/helpers/open_lists/application_helper.rb
@@ -16,6 +16,11 @@ module OpenLists
     def display_other_direction() DIRECTIONS[other_direction.to_sym] end
 
 
+    ## 
+    # @return a (remote) link to invert the current direction of the sort set through the session context in the list display
+    # @param [String] column: the name of the column to sort the list by
+    # @param [String] title: An optional title for the link (if not provided, will be guessed from the column name)
+    # @param [String] title: An optional css class for the generated link
     def sortable_link(column, title: nil, css_class: "")
       column = column.to_s
       title = column.titleize if title.nil?
@@ -24,28 +29,90 @@ module OpenLists
       link_to title, params.merge(sort: column, direction: direction), class: css_class, remote: true, method: :get
     end
 
+    ## 
+    # @return a (remote) bootstrap button to invert the current direction of the sort set through the session context in the list display
+    # @param [String] column: the name of the column to sort the list by
+    # @param [String] title: An optional title for the link (if not provided, will be guessed from the column name)
+    # @param [String] title: An optional css class for the generated link
     def sortable_link_as_button(column, title: nil, css_class: 'btn btn-mini')
       sortable_link column, title: title, css_class: css_class
     end
 
-
+    ##
+    # @return a (remote) link to set the param[:per] value for the number of items displayed in a list
+    # @param [Integer] value: number of item to display in a list
+    # @param [String] css_class: an optional css class for the generated link
     def per_link(value = params[:per], css_class = "")
       value = Kaminari.config.default_per_page if value.nil? #
       link_to value.to_s, params.merge(per: value), class: css_class, remote: true, method: :get
     end
 
-    def per_link_as_button(value = nil)
-      per_link value, 'btn btn-mini'
+    ##
+    # @return a (remote) bootstrap button to set the param[:per] value for the number of items displayed in a list
+    # @param [Integer] value: number of item to display in a list
+    # @param [String] css_class: an optional css class for the generated link
+    def per_link_as_button(value = nil, css_class = 'btn btn-mini')
+      per_link value, css_class
     end
 
+    ##
+    # @return piece of html corresponding to the selector for the number of items to display in a list
+    # @param [Integer] value: number of item to display in a list
     def items_per_page_button_tag(value)
       render partial: 'items_per_page_button', locals: {value: value}
     end
 
-    def column_list_header_button_tag(column)
-      render partial: 'column_list_header_button', locals: {column: column, title: @model.human_attribute_name(column)}
+    ##
+    # @return piece of html corresponding to the header of the column in list display
+    # @param [String] column
+    # @param [Class] model
+    def column_list_header_button_tag(column, model = @model)
+      render partial: 'column_list_header_button', locals: {column: column, title: model.human_attribute_name(column)}
     end
 
+
+    ##
+    # @return a display name for the item. many options there.
+    # It will take the best option:
+    # * if the item itself knows how to be displayed (respond_to? :display_name_for_item), it will be the preferred choice
+    # * if there is a title column, it will use it.
+    # * if there is a name column, it will use it.
+    # * if none of the above, it will display the name of class iten is an instance of follow by the id number
+    # * if there is no id (bad!!), it will diplay "No name"
+    # @param [ActiveRecord::Base] item
+    def display_name_for_item(item)
+      return "" if item.nil?
+      name = I18n.t('.no_name', :default => "No name")
+      name = "#{item.class.display_name} #{item.attributes['id']}" if item.attribute_names.include? 'id'
+      name = item.attributes['name'] if item.attribute_names.include? 'name'
+      name = item.attributes['title'] if item.attribute_names.include? 'title'
+      name = item.class.display_name_for_item(item) if item.class.respond_to? :display_name_for_item
+      name
+    end
+
+    ##
+    # @return an array of editable attributes for this model
+    # REVIEW: shouldn't primary key and timestamps be removed at this level ?
+    # @param [Class] model
+    def fields_to_edit_for_model(model = @model)
+      model.attribute_names
+    end
+
+
+    ##
+    # @return an of the fields to be displayed for this model
+    # @param [Class] model
+    def fields_to_display_for_model(model = @model)
+      fields = model.attr_accessible[:default].to_a - [""]
+      fields = model.attribute_names if fields.empty?
+      fields
+    end
+
+    ##
+    # @return the base url of the engine
+    def module_base_url
+      Engine.routes.default_scope[:module]
+    end
 
   end
 end
--- app/helpers/open_lists/generic_helper.rb
+++ app/helpers/open_lists/generic_helper.rb
@@ -1,49 +1,44 @@
 module OpenLists
   module GenericHelper
+
+    # @return the path for a new item
     def new_item_path
       return "/#{module_base_url}/#{params[:domain]}/#{params[:list_name]}/new" if params[:domain] and params[:list_name]
       "/#{module_base_url}/#{@domain.to_param}"
     end
 
+    # @return the path to the list item passed (default is current)
+    # @param [DynamicModel::ActiveRecordExtension] item
     def item_path(item = @item)
       return "/#{module_base_url}/#{params[:domain]}/#{params[:list_name]}/#{params[:id]}" if params[:domain] and params[:list_name] and params[:id]
       return "#{model_path(item.class)}/#{item.to_param}" unless item.nil?
       nil
     end
 
+    # @return the path to the list index passed (default is current)
+    # @param [Class] model
     def model_path(model = @model)
       "/#{module_base_url}/#{model.to_param}"
     end
 
+    # @return the edit path to the list item passed (default is current)
+    # @param [DynamicModel::ActiveRecordExtension] item
     def edit_item_path(item = @item)
       "#{item_path item}/edit"
     end
 
+
+    # @return provides an option hash for the form tag as the controller is very specific
+    # @param [DynamicModel::ActiveRecordExtension] item
     def form_options(item = @item)
       url = item_path(item)
       {url: url, controller: 'OpenLists::Generic'}
     end
 
-    def name_for_link(item)
-      return "" if item.nil?
-      name = 'No name'
-      name = "#{item.class.display_name} #{item.attributes['id']}" if item.attribute_names.include? 'id'
-      name = item.attributes['name'] if item.attribute_names.include? 'name'
-      name = item.attributes['title'] if item.attribute_names.include? 'title'
-      name = item.class.name_for_link(item) if item.class.respond_to? :name_for_link
-      name
-    end
-
-    def fields_to_edit_for_model(model = @model)
-      model.attribute_names
-    end
-
-    def fields_to_display_for_model(model = @model)
-      fields = model.attr_accessible[:default].to_a - [""]
-      fields = model.attribute_names if fields.empty?
-      fields
-    end
-
+    ## 
+    # @return the html to display the attribute of the item
+    # @param [ActiveRecord::Base] item
+    # @param [String] attribute
     def field_as_display_element(item, attribute)
       attribute_type = item.class.columns_hash[attribute].type
       case attribute_type
@@ -51,7 +46,7 @@ module OpenLists
           # Special case: is it a link to another table
           if item.class.reflections.keys.include? attribute.gsub(DynamicModel::RelationsAnalyser::KEY_IDENTIFIER, '')
             child = item.send attribute.gsub(DynamicModel::RelationsAnalyser::KEY_IDENTIFIER, '').to_sym
-            return link_to(name_for_link(child), item_path(child))
+            return link_to(display_name_for_item(child), item_path(child))
           end
           # Else just show the number
           item.attributes[attribute]
@@ -62,6 +57,11 @@ module OpenLists
       end
     end
 
+    ## 
+    # @return the html to edit the attribute of the item
+    # @param [ActionView::Helpers::FormBuilder] form
+    # @param [Class] model (descendant of ActiveRecord::Base)
+    # @param [String] attribute
     def field_as_edit_element(form, model, attribute)
       attribute_type =  model.columns_hash[attribute].type
       case attribute_type
@@ -72,7 +72,7 @@ module OpenLists
           # if it's any it should end with DynamicModel::RelationsAnalyser::KEY_IDENTIFIER
           reflection_key = attribute.gsub(DynamicModel::RelationsAnalyser::KEY_IDENTIFIER, '')
           if model.reflections.keys.include? reflection_key
-            return form.select(attribute, model.reflections[reflection_key].class_name.constantize.all.map{|s| [ name_for_link(s), s[s.class.primary_key] ]}, {include_blank: 'None'})
+            return form.select(attribute, model.reflections[reflection_key].class_name.constantize.all.map{|s| [ display_name_for_item(s), s[s.class.primary_key] ]}, {include_blank: 'None'})
           end
           # Else just show the number
           form.number_field attribute.to_sym
@@ -88,10 +88,6 @@ module OpenLists
           form.text_field attribute.to_sym
       end
     end
-    def module_base_url
-      Engine.routes.default_scope[:module]
-    end
-
 
   end
 end
