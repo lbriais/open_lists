eaf3e682acca0c544766b45a7cfd37d4a0041e39
Laurent <lbhomeadmin@gmail.com>
2013-05-25 10:42:22 +0200

    Fixed module_path

--- app/helpers/open_lists/domains_helper.rb
+++ app/helpers/open_lists/domains_helper.rb
@@ -2,8 +2,8 @@ module OpenLists
   module DomainsHelper
 
     def domain_path(domain = @domain)
-      domain
-      domain.prefix unless domain.class == String
+      path = (domain.class == String) ? domain : domain.prefix
+      "/#{module_base_url}/#{path}"
     end
   end
 end
