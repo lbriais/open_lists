612a79628ce20abba4551f9451216591d3773abc
Laurent <lbnetid+rb@gmail.com>
2013-06-04 14:42:24 +0200

    Display of the domain lists (ie library)

--- app/models/open_lists/domain.rb
+++ app/models/open_lists/domain.rb
@@ -1,12 +1,31 @@
 module OpenLists
   class Domain < ActiveRecord::Base
-    include DynamicModel::DomainExtension
     attr_accessible :description, :name
 
     after_commit :rescan_domain
 
     def rescan_domain
       DynamicModel::introspect name
+    end
+
+    def empty?
+      scoped_table_names.blank?
+    end
+
+    def scoped_table_names
+      domain_module  = DynamicModel::ManagedDomains.domain_module name
+      if domain_module
+        return domain_module.scoped_table_names
+      end
+      nil
+    end
+
+    def model_classes
+      domain_module  = DynamicModel::ManagedDomains.domain_module name
+      if domain_module
+        return domain_module.model_classes
+      end
+      nil
     end
 
   end
